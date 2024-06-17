90965dc359edb82847e71bb257a8e9b313d1f407
eungikim <eungikim.me@gmail.com>
2024-06-17 16:21:38 +0900

    feat: chapter4

---
+++ chapter4/sample-test/index.js
@@ -0,0 +1,9 @@
+const calc = require('sample-package'); // 1.sample-package 불러괴
+
+const a = 18;
+const b = 12;
+
+console.log('a+b = ', calc.add(a, b));
+console.log('a-b = ', calc.sub(a, b));
+console.log('a*b = ', calc.multi(a, b));
+console.log('a/b = ', calc.div(a, b));
